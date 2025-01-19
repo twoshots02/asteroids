1644bc25b368dd9c54e4aa2ea4ea42df725a9e5f
twoshots02 <twoshots02@gmail.com>
2025-01-18 23:21:39 -0600

    added FPS limiting

--- main.py
+++ main.py
@@ -10,15 +10,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-
+# set screen resolution
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# set clock limiter for FPS limiting
+    clock = pygame.time.Clock()
+    dt = 0
 
 # game loop
     while game_running:
+        #allows for the close button on the window to quit the program
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return        
 
+        dt = clock.tick(60)
 
 if __name__ == "__main__":
     main()    
