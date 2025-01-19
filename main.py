102d65b176eab7c5b57dd87d23574f1ff3d4e2ae
twoshots02 <twoshots02@gmail.com>
2025-01-18 23:16:17 -0600

    First window created

--- main.py
+++ main.py
@@ -5,9 +5,20 @@ import pygame
 from constants import *
 
 def main():
+    game_running = True
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# game loop
+    while game_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return        
+
 
 if __name__ == "__main__":
     main()    
